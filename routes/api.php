0251fab62f9f606b6cac01055fc9905871ccb0d9
horvathg13 <horvathgabor338@gmail.com>
2024-05-19 12:33:24 +0200

    Middleware based permission check in api route.

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\{
     RegisterController,
     AuthController,
-    PermissionController,
     UserController,
     ProjectController,
     TaskController,
@@ -32,14 +31,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 /*RegisterController*/
 Route::controller(RegisterController::class)->group(function (){
+    Route::middleware(['managementAuth'])->group(function (){
+        Route::post("createuser","createUser");
+        Route::post("password-reset-manual","passwordResetManual");
+        Route::post("ban-user","banUser");
+    });
     Route::post("register","register");
-    Route::post("createuser","createUser");
-    Route::get("findUser/{token} ","findUser");
     Route::post("resetpassword", "resetPassword");
-    Route::post("password-reset-manual/{id}","passwordResetManual");
-    Route::post("ban-user/{id}","banTheUser");
+    Route::get("findUser/{token} ","findUser");
 });
-
 
 /*AuthController*/
 Route::controller(AuthController::class)->group(function () {
@@ -47,51 +47,54 @@ Route::controller(AuthController::class)->group(function () {
     Route::post("logout", "logout");
 });
 
-/*PermissionController*/
-Route::controller(PermissionController::class)->group(function () {
+/*UserController*/
+Route::controller(UserController::class)->group(function () {
+    Route::middleware(['managementAuth'])->group(function (){
+        Route::post("user-to-role", "userToRole");
+        Route::post("getusers", "getUsers");
+        Route::post("getEmployees", "getEmployees");
+        Route::post("getManagers","getManagers");
+    });
+    Route::post("getUserData","getUserData");
+    Route::post("save-profile-data", "saveProfileData");
     Route::post("getUserRole", "getUserRole");
     Route::post("getroles", "getRoles");
 });
 
-/*UserController*/
-Route::controller(UserController::class)->group(function () {
-    Route::post("getusers", "getUsers");
-    Route::post("getUserData","getUserData");
-    Route::post("getEmployees", "getEmployees");
-    Route::post("getManagers","getManagers");
-    Route::post("user-to-role", "userToRole");
-    Route::post("save-profile-data", "saveProfileData");
-});
-
 /*ProjectController*/
 Route::controller(ProjectController::class)->group(function () {
-    Route::post("createproject", "createProject");
+    Route::middleware(['managementAuth'])->group(function (){
+        Route::post("createproject", "createProject");
+        Route::post("createparticipants", "createParticipants");
+        Route::post("get-manager-projects", "getManagerProjects");
+    });
     Route::post("getprojects","getProjects");
     Route::post("projects/{id}", "getProjectById");
     Route::post("getprojectparticipants/{id}", "getProjectParticipants");
-    Route::post("createparticipants", "createParticipants");
     Route::post("add-favorite-project", "addFavoriteProject");
     Route::post("remove-favorite-project", "removeFromFavorite");
     Route::post("get-favorite-projects", "getFavoriteProjects");
-    Route::post("get-manager-projects", "getManagerProjects");
     Route::post("leave-project", "leaveProject");
 });
 
 /*TaskController*/
 Route::controller(TaskController::class)->group(function () {
-    Route::post("createtask", "createtask");
+    Route::middleware(['taskManagementAuth'])->group(function (){
+        Route::post("assign-employee-to-task","AssignEmpoyleeToTask");
+        Route::post("get-manager-tasks", "getManagerTasks");
+        Route::post("managed-completed-tasks", "managedCompletedTasks");
+        Route::post("accept-all-task", "acceptAllTask");
+    });
+    Route::middleware(['projectParticipantAuth'])->group(function (){
+        Route::post("projects/{id}/tasks", "getTasks");
+        Route::post("createtask", "createtask");
+    });
     Route::post("getpriorities", "getPriorities");
-    Route::post("projects/{id}/tasks", "getTasks");
-    Route::post("assign-employee-to-task","AssignEmpoyleeToTask");
     Route::post("task-attach-to-myself", "AttachMyself");
     Route::post("getActiveEmployees/{task_id}","getActiveEmployees");
     Route::post("completed", "Completed");
     Route::post("get-my-tasks",  "MyTasks");
-    Route::post("get-manager-tasks", "getManagerTasks");
-    Route::post("managed-completed-tasks", "managedCompletedTasks");
-    Route::post("accept-all-task", "acceptAllTask");
     Route::post("count-of-my-active-tasks", "countOfMyTasks");
-    Route::post("accessControlForTasks", "AccessControlForTasks");
 });
 
 /*MessageController*/
@@ -105,19 +108,24 @@ Route::controller(MessageController::class)->group(function () {
 Route::controller(ButtonController::class)->group(function () {
     Route::post("get-buttons/{projecId}", "getProjectandTaskButtons");
     Route::post("get-users-buttons", "getUsersButton");
+    Route::post("buttonAuth", "buttonAuth");
 });
 
 /*StatusController*/
 Route::controller(StatusController::class)->group(function () {
+    Route::middleware(['managementAuth'])->group(function (){
+        Route::post("set-status", "setStatus");
+    });
     Route::post("get-status/{ProjectId}/{TaskId}", "getStatus");
-    Route::post("set-status", "setStatus");
     Route::post("filter-status/{ProjectId}/{Task}/{StatusId}", "statusFilterProjectOrTask");
 });
 
 /*NotificationController*/
 Route::controller(NotificationController::class)->group(function () {
+    Route::middleware(['managementAuth'])->group(function (){
+        Route::post("get-manager-notification", "getManagerNotification");
+    });
     Route::post("notifications", "Notifications");
-    Route::post("get-manager-notification", "getManagerNotification");
 });
 
 
